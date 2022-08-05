8a1830ce59af503ad616a2bd4088dd0f78d859a8
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-08-05 21:09:22 +0500

    Added routing for createListing page

--- src/App.js
+++ src/App.js
@@ -12,28 +12,30 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Category from './pages/Category';
+import CreateListing from './pages/CreateListing';
 
 function App() {
-  return (
-    <div>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Explore />} />
-                <Route path="/offers" element={<Offers />} />
-                <Route path="/profile" element={<PrivateRoute />} >
-                    <Route path="/profile" element={<Profile />} />
-                </Route>
-                <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/category/:categoryName" element={<Category/>} />
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Explore />} />
+                    <Route path="/offers" element={<Offers />} />
+                    <Route path="/profile" element={<PrivateRoute />} >
+                        <Route path="/profile" element={<Profile />} />
+                    </Route>
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/create-listing" element={<CreateListing/>} />
+                    <Route path="/category/:categoryName" element={<Category/>} />
 
-            </Routes>
-            <NavBar/>
-        </Router>
-        <ToastContainer></ToastContainer>
-    </div>
-  );
+                </Routes>
+                <NavBar/>
+            </Router>
+            <ToastContainer></ToastContainer>
+        </div>
+    );
 }
 
 export default App;
