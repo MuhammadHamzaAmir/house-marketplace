edd5b91cc0a3b603c9acb5fcc0efb54e81d46a92
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-08-06 01:05:25 +0500

    Images are uploaded to firebase storage

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import {toast} from "react-toastify";
+import {getStorage, ref, getDownloadURL, uploadBytesResumable} from "firebase/storage";
+import {db} from "../firebase.config"
+import {v4 as uuidv4} from "uuid";
 
 function CreateListing() {
     const [geoLocationEnabled, setGeoLocationEnabled] = useState(false);
@@ -64,26 +68,73 @@ function CreateListing() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isMounted, auth, navigate]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
-        const listing = {
-            name,
-            type,
-            regularPrice,
-            discountedPrice,
-            bathrooms,
-            bedrooms,
-            offer,
-            parking,
-            furnished,
-            location,
-            imageUrls,
-            lat,
-            lng,
-            userRef: formData.userRef,
-            timestamp: new Date().toISOString(),
-        };
+
+        if(discountedPrice > regularPrice) {
+            setLoading(false);
+            toast.error("Discounted price cannot be greater than regular price");
+            return;
+        }
+        if(imageUrls.length>6){
+            setLoading(false);
+            toast.error("You can only upload a maximum of 6 images");
+            return;
+        }
+
+        let geoloation = {};
+        geoloation.lng = lng;
+        geoloation.lat = lat;
+
+        // store image in firebase storage
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage();
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+                const storageRef = ref(storage,'images/'+fileName);
+                const uploadTask = uploadBytesResumable(storageRef, image);
+                
+                uploadTask.on("state_changed", (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Upload is " + progress + "% done");
+                    switch (snapshot.state) {
+                        case "paused":
+                            console.log("Upload is paused");
+                            break;
+                        case "running":
+                            console.log("Upload is running");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                , (error) => {
+                    reject(error);
+                }
+                , () => {
+                    getDownloadURL().then(function(url) {
+                        resolve(url);
+                    }).catch(function(error) {
+                        reject(error);
+                    })
+                })
+            });
+        }
+        const imgUrls = await Promise.all(
+            [...imageUrls].map(async (image) => {storeImage(image)})
+        ).catch((error) => {
+            setLoading(false);
+            toast.error("Error uploading images");
+            return;
+        });
+
+        console.log(imgUrls);
+        console.log(imageUrls);
+        console.log(formData);
+        setLoading(false);
+
+        
     };
 
     const onMutate = (e) => {
